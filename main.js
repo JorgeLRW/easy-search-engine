de0cdfb3c2eede55141674ccbe783c444239bf1c
JorgeLRW <108426729+JorgeLRW@users.noreply.github.com>
2024-07-31 15:33:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function overlay() {
+    const overlay = document.getElementById('search');
+    overlay.classList.add('overlay');
+};
+
+function closeOverlay() {
+    const overlay = document.getElementById('search');
+    overlay.classList.remove('overlay');
+};
